d231ccda8df08a36e2649ebe59f66bebb74bd779
Keith Whitson <keith.whitson@franka.de>
2021-12-03 14:44:13 +0100

    did the whole react thing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import {increment, decrement} from './actions';
 
 function App() {
+  const counter = useSelector( state => state.counter);
+  const isLogged = useSelector( state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => {dispatch(increment())}}>
+        +
+      </button>
+      <button onClick={() => {dispatch(decrement())}}>
+        -
+      </button>
+      {isLogged && <h3>Valuable information I shouldnt see if i am not logged in</h3>}
     </div>
   );
 }
